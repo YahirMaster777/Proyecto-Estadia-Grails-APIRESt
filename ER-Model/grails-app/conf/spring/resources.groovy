22cc0ea27cb32eecd32d49029cbb6e53661721cc
Yair Tolentino <92339589+YahirMaster777@users.noreply.github.com>
2025-02-05 16:40:42 -0600

    Delete ER-Model directory

--- ER-Model/grails-app/conf/spring/resources.groovy
+++
@@ -1,5 +0,0 @@
-import com.ordenaris.internalControl.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener)
-}
--- ER-Model/grails-app/init/er/model/BootStrap.groovy
+++
@@ -1,9 +0,0 @@
-package er.model
-
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
-}
