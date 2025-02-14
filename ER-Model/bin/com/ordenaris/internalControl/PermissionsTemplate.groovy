dc02936c2b0ad483be10ef1587e5780be96ae950
MarvinYairTolentino <yahirperez141002@gmail.com>
2025-02-14 17:50:32 -0600

    Correcciones en empresa y permisos-perfil

--- ER-Model/bin/com/ordenaris/internalControl/PermissionsTemplate.groovy
+++ ER-Model/bin/com/ordenaris/internalControl/PermissionsTemplate.groovy
@@ -1,10 +1,10 @@
 package com.ordenaris.internalControl
 
 class PermissionsTemplate {
-    String uuid = UUID.randomUUID().toString().replaceAll('\\-','')
     Date dateCreated
     Date lastUpdated
     Sections section
+    String description
     Permissions permission
     Templates template
     
