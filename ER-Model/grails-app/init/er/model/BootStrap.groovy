db7ab71145c7ab1cbaaf919220ee0c087ce31003
emilio-MC-ord <emilio.mendoza@ordenaris.com>
2025-02-17 17:35:07 -0600

    creación de usuarios

--- ER-Model/grails-app/init/er/model/BootStrap.groovy
+++ ER-Model/grails-app/init/er/model/BootStrap.groovy
@@ -3,9 +3,27 @@ package er.model
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import groovy.json.JsonBuilder;
+import com.ordenaris.internalControl.Users;
+import com.ordenaris.internalControl.UsersRoles;
+import com.ordenaris.internalControl.Roles;
 
 class BootStrap {
     def init = { servletContext ->
+        if (Roles.count() == 0) {
+            def roleAdmin = new Roles(authority: 'ROLE_ADMIN').save(flush: true)// flush: true para que se guarde en la base de datos
+            def roleRoot = new Roles(authority: 'ROLE_ROOT').save(flush: true)// flush: true para que se guarde en la base de datos
+            def roleVendedor = new Roles(authority: 'ROLE_CUSTOM').save(flush: true)// flush: true para que se guarde en la base de datos
+            def userRoot1 = new Users(username: 'yair.ordenaris@gmail.com', password: 'Yair141002')
+            def userRoot2= new Users(username: 'emilio.ordenaris@gmail.com', password: '1a2b3c4d')
+            if (!userRoot1.save(flush: true) || !userRoot2.save(flush: true)) {
+                userRoot1.errors.allErrors.each { println it }
+                userRoot2.errors.allErrors.each { println it }
+            } else {
+                new UsersRoles(user: userRoot1, role: roleRoot).save(flush: true)
+                new UsersRoles(user: userRoot2, role: roleRoot).save(flush: true)
+            }
+        }
+        
         String.metaClass.validFormatDataHour = {
             def horaCodeExpression = '([0-1][1-9]|[2][0-3])(:)([0-5][0-9])$'
             def pattern = Pattern.compile(horaCodeExpression) 
