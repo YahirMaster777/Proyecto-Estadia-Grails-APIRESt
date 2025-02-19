d85151d54550c95508b48714051f4f2dafd0d7a6
emilio-MC-ord <emilio.mendoza@ordenaris.com>
2025-02-19 10:23:49 -0600

    integración de inserciones por defecto en el boostrap

--- ER-Model/grails-app/controllers/er/model/UrlMappings.groovy
+++ ER-Model/grails-app/controllers/er/model/UrlMappings.groovy
@@ -3,7 +3,7 @@ package er.model
 class UrlMappings {
 
     static mappings = {
-            group "/control", {
+            group "/private", {
                 group "/create", {
                     post "/app"(controller:'apps', action: 'create')
                     post "/user"(controller:'users', action: 'create')
