5887071f29b1f8736e496a22d12d668606ee40ed
emilio-MC-ord <emilio.mendoza@ordenaris.com>
2025-03-11 16:09:03 -0600

    Actualización del usuario

---
+++ ER-Model/grails-app/controllers/com/ordenaris/internalControl/SettingsController.groovy
@@ -0,0 +1,11 @@
+package com.ordenaris.internalControl
+
+
+import grails.rest.*
+import grails.converters.*
+
+class SettingsController {
+	static responseFormats = ['json', 'xml']
+	
+    def index() { }
+}
--- ER-Model/grails-app/services/com/ordenaris/internalControl/UsersService.groovy
+++ ER-Model/grails-app/services/com/ordenaris/internalControl/UsersService.groovy
@@ -232,17 +232,15 @@ class UsersService {
     
     def sections(username) {
         def sectionPermissionList = [:]
-        UserSectionPermission.findAllByUser(username).each { templatePermission ->
+        def user= UserSectionPermission.findAllByUser(username).each { templatePermission ->
             def section = templatePermission.permission.section
             if (!sectionPermissionList.containsKey(section.name)) {
                 sectionPermissionList[section.name] = [:]
             }
-                
-            // sectionPermissionList[section.name] [templatePermission.permission.name] = templatePermission.permission.alias  
-            sectionPermissionList[section.name][templatePermission.permission.name] = templatePermission.permission.alias
+            sectionPermissionList[section.name]
         }
         def section = sectionPermissionList.collect { nameSection, permiss ->
-            return [section: nameSection, permisos: permiss]
+            return [section: nameSection, permisos: user?.permission.alias ]
         }
         return section
     }
