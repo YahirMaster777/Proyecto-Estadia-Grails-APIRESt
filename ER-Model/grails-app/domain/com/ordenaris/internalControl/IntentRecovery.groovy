5a7236f9dcf607f080af92adda218bfb6abccf9d
emilio-MC-ord <emilio.mendoza@ordenaris.com>
2025-03-11 13:17:15 -0600

    cambios para el login

--- ER-Model/grails-app/domain/com/ordenaris/internalControl/IntentRecovery.groovy
+++ ER-Model/grails-app/domain/com/ordenaris/internalControl/IntentRecovery.groovy
@@ -3,7 +3,6 @@ package com.ordenaris.internalControl
 class IntentRecovery {
     Date dateCreated
     String uuid = UUID.randomUUID().toString().replaceAll('\\-', '')
-    String host
     String used = 'Activo'
     Users user
     int intents
@@ -14,7 +13,6 @@ class IntentRecovery {
         used sqlType:"Enum('Activo', 'Inactivo')"
     }
     static constraints = {
-        host blank:true, nullable:true,maxSize:15
         used inList:['Activo', 'Inactivo']
         uuid maxSize:32, unique:true
         dateExpired nullable:true, blank:true
--- ER-Model/grails-app/domain/com/ordenaris/internalControl/Settings.groovy
+++ ER-Model/grails-app/domain/com/ordenaris/internalControl/Settings.groovy
@@ -10,4 +10,36 @@ class Settings {
     static mapping = {
         version false
     }
+    // public static final NUMBER_OF_RECOVERY_ATTEMPTS = new Settings(data: '3', identifier: 'NUMBER_OF_RECOVERY_ATTEMPTS').save(flush:true)
+    // public static final MINUTES_OF_VALIDITY_CODE = new Settings(data: '30', identifier: 'MINUTES_OF_VALIDITY_CODE').save(flush:true)
+    // public static void initializeDefaults() {
+    //     if (!Settings.findByIdentifier('NUMBER_OF_RECOVERY_ATTEMPTS')) {
+    //         .save(flush: true, failOnError: true)
+    //     }
+    //     if (!Settings.findByIdentifier('MINUTES_OF_VALIDITY_CODE')) {
+    //        .save(flush: true, failOnError: true)
+    //     }
+    // }
+
+    public static initializeDefaults() {
+        def registers = [
+            new Settings(data: '3', identifier: 'NUMBER_OF_RECOVERY_ATTEMPTS'),
+            new Settings(data: '30', identifier: 'MINUTES_OF_VALIDITY_CODE')
+        ]
+        Settings.saveAll(registers)
+    }
+    
+    def SettingsService
+    public static Settings createSetting(String identifier, String data) {
+        SettingsService.createSetting(identifier, data)
+    }
+    public static Settings updateSetting(String identifier, String data) {
+        SettingsService.updateSetting(identifier, data)        
+    }
+    public static void deleteSetting(String identifier) {
+        SettingsService.deleteSetting(identifier)
+    }
+    public static void refreshData() {
+        SettingsService.refreshData()
+    }
 }
